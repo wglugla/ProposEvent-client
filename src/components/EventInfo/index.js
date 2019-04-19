fb34f101ba5e0c239c6629f87ce322e0aff7a15a
wglugla <gluglawojciech@gmail.com>
2019-04-19 08:43:01 +0200

    Add info about event's tags

--- src/components/EventInfo/index.js
+++ src/components/EventInfo/index.js
@@ -3,7 +3,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const EventInfo = props => {
-  const { title, place, date, description } = props.event;
+  const { title, place, date, description, event_tags } = props.event;
+  console.log('PROPS EVENT: ', props.event);
   return (
     <li>
       <Link to={`/events/${props.event.event_id}`}>
@@ -11,6 +12,11 @@ const EventInfo = props => {
         <p> Miejsce: {place} </p>
         <p> Data: {date} </p>
         <p> Opis: {description} </p>
+        <ul>
+          {JSON.parse(event_tags).map(tag => (
+            <li key={tag}> {tag} </li>
+          ))}
+        </ul>
         <p> -------------------- </p>
       </Link>
     </li>
